a2b148d124a8ab61ea7abbf6307ec834a1b65ec9
Pablo Ariel De Los Santos <pdelossantos@nosis.com>
2025-07-22 17:25:09 -0300

    indice de cadenas

---
+++ strings/indices_cadenas.py
@@ -0,0 +1,9 @@
+# Manejo de indices en una cadena
+cadena1 = "Hola Mundo"
+print(cadena1)
+
+# Recupoerar el primer caracter
+primer_caracter = cadena1[0]
+print(primer_caracter)
+ultimo_caracter = cadena1[9]
+print(ultimo_caracter)
