7ead9ac7d50bd6dff5b5b679cd1cc9e84bd35ae2
Pablo Ariel De Los Santos <pdelossantos@nosis.com>
2025-07-22 17:11:31 -0300

    manejo de cadenas

---
+++ strings/manejo_cadenas.py
@@ -0,0 +1,11 @@
+# Cadenas en Python
+cadena1 = 'Hola Mundo'
+cadena1 = 'Adios'
+cadena2 = "Python en genial "
+cadena3 = '''Este es un ejemplo
+  de múltiples líneas
+  en una cadena'''
+
+print(cadena1)
+print(cadena2)
+print(cadena3)
