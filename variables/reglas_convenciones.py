e97698749890a3c5a0618cc6f4fb705bb7fc1a04
Pablo Ariel De Los Santos <pdelossantos@nosis.com>
2025-07-22 11:09:44 -0300

    Mover reglas_convenciones.py fuera del entorno virtual

---
+++ variables/reglas_convenciones.py
@@ -0,0 +1,21 @@
+# Reglas y conveciones en nombres de variables
+
+# Ejemplos de reglas estrictas
+nombre_usuario = "Juan Pérez"
+# 1nombre_usuario = "Juan Pérez"
+
+# No podemos usar palabras reservadas
+# class = "mi clase"
+
+# Sensibles a mayúsculas y minúsculas
+nombre = "Juan"
+Nombre = "Karla"
+print(nombre)
+print(Nombre)
+
+# snake case
+nombre_completo = "Pablo de los Santos"
+
+# Prefijos y sifijos
+es_casado = True
+nombre_txt = "archivo.txt"
---
+++ variables/variables.py
@@ -0,0 +1,30 @@
+# Variables en Python
+
+# Declaración e inicialización de variables
+
+edad = 28
+altura = 1.65
+pais = "México"
+
+# Acceder a las variables
+print("Valores iniciales")
+print("Edad:", edad)
+print("Altura:", altura)
+print("País:", pais)
+
+# Modificar el valor de una variable
+edad = 30
+altura = 1.68
+
+print("Valores modificados")
+print("Edad:", edad)
+print("Altura:", altura)
+print("País:", pais)
+
+# En python el tipo es dinámico
+edad = "treinta"
+print("Edad:", edad)
+
+# Si queresmo acceder a una variable no declarada manda error
+telefono = "55443322"
+print("Telefono:", telefono)
