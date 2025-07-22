5825b64c66e1c76774dd182a6721350fb0d44909
Pablo Ariel De Los Santos <pdelossantos@nosis.com>
2025-07-22 15:50:14 -0300

    Reto 2. Sistema de tienda online

---
+++ reto_02/tienda_online.py
@@ -0,0 +1,24 @@
+# Sistema de tienda online
+titulo = "*** Sistema de tienda online ***"
+print(titulo)
+
+nombre_producto = "Cámara digital"
+precio_producto = (399.99)
+cantidad_inventario = 20
+disponible_entrega = True
+
+print("Producto:", nombre_producto)
+print("Precio: $", precio_producto)
+print("Cantidad en el inventario:", cantidad_inventario)
+print("Disponible para entrega:", disponible_entrega)
+
+nombre_producto = "Notebook"
+precio_producto = (699.99)
+cantidad_inventario = 5
+disponible_entrega = True
+
+print()
+print("Producto:", nombre_producto)
+print("Precio: $", precio_producto)
+print("Cantidad en el inventario:", cantidad_inventario)
+print("Disponible para entrega:", disponible_entrega)
