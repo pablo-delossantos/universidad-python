45520c92cb64be86d2b944e3461851bb55308833
Pablo de los Santos <estudiodedalo@gmail.com>
2025-07-26 23:01:52 -0300

    Inmiutabilidad de cadenas

--- strings/inmutabilidad_cadenas.py
+++ strings/inmutabilidad_cadenas.py
@@ -1,4 +1,7 @@
 # Inmutabiliadad en cadenas
 cadena1 = "Hola Mundo"
-# cadena1[0] = "h"
-# print(cadena1)
+# cadena1[0] = "h"  No podemos modificar los caracteres
+cadena2 = cadena1
+cadena1 = "Adios"
+print(cadena1)
+print(cadena2)
